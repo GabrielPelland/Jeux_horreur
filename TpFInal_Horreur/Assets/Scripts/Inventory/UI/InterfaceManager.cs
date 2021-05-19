00e6bd2b781f4c4c38f3870af418006e833ae52d
GabrielPelland <gabrielpelland@hotmail.com>
2021-05-19 15:32:42 -0400

    UI_v4

---
+++ TpFInal_Horreur/Assets/Scripts/Inventory/UI/InterfaceManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceManager : MonoBehaviour
+{
+    public GameObject notificationAttention;
+    public GameObject notificationObjectif;
+    public GameObject notificationMort;
+    public GameObject notificationFin;
+
+    public GameObject uiTouches;
+
+    public GameObject positionTop;
+    public GameObject positionCenter;
+}
